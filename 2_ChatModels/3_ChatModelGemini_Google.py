4fde57ca55b490b89d91344063456aadb545d21f
shailesh <shaileshvbs0@gmail.com>
2025-08-07 17:01:07 +0530

    removed google api key

--- 2_ChatModels/3_ChatModelGemini_Google.py
+++ 2_ChatModels/3_ChatModelGemini_Google.py
@@ -1,7 +1,9 @@
 from langchain_google_genai import GoogleGenerativeAI
 from dotenv import load_dotenv
 
-model=GoogleGenerativeAI(model='gemini-1.5-pro',api_key="AIzaSyB3_IaaarjEgTyQ8UQc9gZ2nv5IbMx-rig")
+loadenv=load_dotenv()
+api_key=loadenv.get("GOOGLE_API_KEY")
+model=GoogleGenerativeAI(model='gemini-1.5-pro',api_key=api_key)
 
 result=model.invoke("what is the capital of india")
 
